cfb33621223aeb3e0cfbc7dee07538d47be37827
DuongLee98 <duong98.lebinh@gmail.com>
2020-03-30 22:13:04 +0700

    new ud

--- src/View/LeDuong/Calculate.java
+++ src/View/LeDuong/Calculate.java
@@ -210,7 +210,7 @@ public class Calculate extends javax.swing.JFrame {
         jLabel7.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel7.setText("Tính tiền điện");
 
-        jButton1.setText("In Hóa Đơn");
+        jButton1.setText("Tạo Hóa Đơn");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
