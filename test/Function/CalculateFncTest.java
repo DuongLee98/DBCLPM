826e38d97ed1e3ee90aad9a46ea86c8a1547588a
DuongLee <DuongLee@DESKTOP-59177LS>
2020-06-28 21:54:19 +0700

    ud

--- test/Function/CalculateFncTest.java
+++ test/Function/CalculateFncTest.java
@@ -204,7 +204,6 @@ public class CalculateFncTest {
         }
         
         double tax = 10.0;
-        //Double expResult = (double)187660;
         Double result;
         try {
             result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
@@ -213,9 +212,126 @@ public class CalculateFncTest {
             String expResult = "Wrong level<0 in arrayLevel index = 0";
             assertEquals(expResult, ex.getMessage());
         }
-        //assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testCalculatePrice6_1(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, -50, 100, 100, 100, -1};
+        int[] arrPriceInp = {1678, 1734, 2014, 2536, 2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong level<0 in arrayLevel index = 1";
+            assertEquals(expResult, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCalculatePrice6_2(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, -100, 100, 100, -1};
+        int[] arrPriceInp = {1678, 1734, 2014, 2536, 2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong level<0 in arrayLevel index = 2";
+            assertEquals(expResult, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCalculatePrice6_3(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, 100, -100, 100, -1};
+        int[] arrPriceInp = {1678, 1734, 2014, 2536, 2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong level<0 in arrayLevel index = 3";
+            assertEquals(expResult, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCalculatePrice6_4(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, 100, 100, -100, -1};
+        int[] arrPriceInp = {1678, 1734, 2014, 2536, 2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong level<0 in arrayLevel index = 4";
+            assertEquals(expResult, ex.getMessage());
+        }
     }
     
     @Test
@@ -238,7 +354,6 @@ public class CalculateFncTest {
         }
         
         double tax = 10.0;
-        //Double expResult = (double)187660;
         Double result;
         try {
             result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
@@ -247,9 +362,156 @@ public class CalculateFncTest {
             String expResult = "Wrong arrPrice<0 in arrPrice index = 0";
             assertEquals(expResult, ex.getMessage());
         }
-        //assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testCalculatePrice7_1(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, 100, 100, 100, -1};
+        int[] arrPriceInp = {1678, -1734, 2014, 2536, 2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong arrPrice<0 in arrPrice index = 1";
+            assertEquals(expResult, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCalculatePrice7_2(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, 100, 100, 100, -1};
+        int[] arrPriceInp = {1678, 1734, -2014, 2536, 2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong arrPrice<0 in arrPrice index = 2";
+            assertEquals(expResult, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCalculatePrice7_3(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, 100, 100, 100, -1};
+        int[] arrPriceInp = {1678, 1734, 2014, -2536, 2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong arrPrice<0 in arrPrice index = 3";
+            assertEquals(expResult, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCalculatePrice7_4(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, 100, 100, 100, -1};
+        int[] arrPriceInp = {1678, 1734, 2014, 2536, -2834, 2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong arrPrice<0 in arrPrice index = 4";
+            assertEquals(expResult, ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCalculatePrice7_5(){
+        System.out.println("CalculatePrice");
+        int total = 200;
+        int[] arrLevelInp = {50, 50, 100, 100, 100, -1};
+        int[] arrPriceInp = {1678, 1734, 2014, 2536, 2834, -2927};
+        
+        ArrayList<Integer> arrLevel = new ArrayList<>();
+        for (int i=0; i<arrLevelInp.length; i++)
+        {
+            arrLevel.add(arrLevelInp[i]);
+        }
+        ArrayList<Integer> arrPrice = new ArrayList<>();
+        for (int i=0; i<arrPriceInp.length; i++)
+        {
+            
+            arrPrice.add(arrPriceInp[i]);
+        }
+        
+        double tax = 10.0;
+        Double result;
+        try {
+            result = CalculateFnc.CalculatePrice(total, arrLevel, arrPrice, tax);
+            fail( "Should have thrown an exception" );
+        } catch (Exception ex) {
+            String expResult = "Wrong arrPrice<0 in arrPrice index = 5";
+            assertEquals(expResult, ex.getMessage());
+        }
     }
     
     /**
@@ -281,13 +543,10 @@ public class CalculateFncTest {
             expResult.add(oup[i]);
         ArrayList<Integer> result = CalculateFnc.stringToListInteger(data);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
     
     @Test
     public void testStringToListInteger2() throws Exception {
-        //Test 1678|1734|2014|2536|2834|2927
         System.out.println("stringToListInteger");
         try
         {
@@ -300,8 +559,6 @@ public class CalculateFncTest {
             String expResult = "Throw exception data split not the same size";
             assertEquals(expResult, e.getMessage());
         }
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
     
     @Test
@@ -319,7 +576,5 @@ public class CalculateFncTest {
             String expResult = "Throw exception parseInt";
             assertEquals(expResult, e.getMessage());
         }
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
 }
